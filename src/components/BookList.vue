afb01d82fa29cf4688b6e55d3ee85c1ff3025878
Iosif Balasca <c17719309@mydit.ie>
2020-02-21 14:24:16 +0000

    Completed module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>{{title}}</h1>
         <ul>
-            <li v-for="book in books">{{book.title}}:{{book.author}}</li> 
+            <li>{{book.title}}:{{book.author}}</li> 
         </ul>
 
     </div>
@@ -32,8 +32,5 @@ ul{
     list-style-type: none;
     padding: 0;
 }
-li{
-    display: block;
-    margin: 0 10px;
-}
+
 </style>
