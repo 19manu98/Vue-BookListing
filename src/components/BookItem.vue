68b3614c5029e01caac3f011a5321502751e768d
Iosif Balasca <c17719309@mydit.ie>
2020-02-21 14:43:44 +0000

    Completed module

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -1,11 +1,11 @@
 <template>
-    <li v-for="book in books" >{book.title}:{book.author}</li>
+    <li>{book.title}:{book.author}</li>
 </template>
 
 <script>
 export default {
     name: 'BookItem',
-    props: ['book']
+    props: ['book'],
 }
 </script>
 
