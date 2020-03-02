05e95b3b5b55fb0bfcb05238cad831696dbbabc5
Iosif Balasca <c17719309@mydit.ie>
2020-03-02 12:29:17 +0000

    Completed module

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <form v-on:submit.prevent="bookSubmit()" class="" action="#" method="post" >
+    <form v-on:submit.prevent="bookSubmit(bookTitle,bookAuthor)" class="" action="#" method="post" >
         <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
         <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
         <button type="submit" name="button"  >Add Book</button>
@@ -17,8 +17,8 @@ export default {
         }
     },
     methods: {
-        bookSubmit: function(){
-            console.log($bookTitle)
+        bookSubmit(bookTitle,bookAuthor){
+            this.$emit('addBook', bookTitle, bookAuthor);
         }
     },
 }
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -29,7 +29,7 @@ export default {
         }
     },
     methods: {
-        appendBook: function($bookTitle,$bookAuthor){
+        appendBook(bookTitle,bookAuthor){
             this.books.push({ title: bookTitle, author: bookAuthor }) 
         }
     },
