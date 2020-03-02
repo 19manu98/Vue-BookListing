7a411192aa999b59fe56d6fc0cf22fb7ad3e3c03
Iosif Balasca <c17719309@mydit.ie>
2020-03-02 11:24:36 +0000

    Completed module

---
+++ src/components/BookForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <form v-on:submit.prevent="bookSubmit()" class="" action="#" method="post" >
+        <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
+        <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+        <button type="submit" name="button"  >Add Book</button>
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'BookForm',
+    props: ['book'],
+    data() {
+        return {
+            bookTitle: "",
+            bookAuthor: ""
+        }
+    },
+    methods: {
+        bookSubmit: function(){
+            console.log($bookTitle)
+        }
+    },
+}
+</script>
+
+<style scoped>
+li{
+    display: block;
+    margin: 0 10px;
+}
+</style>
